4f5216de4527f3c1bfd090217eee37866c1385a4
Arthur Dornelles <arthur_dornelles19@hotmail.com>
2022-03-27 15:35:09 -0300

    readjusted the files

--- REAME.md
+++ REAME.md
@@ -1,1 +1,7 @@
-Projeto designado a execução do Projeto Final de Curso da Engenharia Física
+Projeto designado a execução do Projeto Final de Curso da Engenharia Física.
+
+main.jl - usará o modulo ConfigParameters dentro de Config.jl para criar as iterações e salvar dentro de data/iterations/.
+
+animation.jl - usará as iterações criadas por main.jl para criar animações das iterações e do fluxo em output/.
+
+sampling.jl - usará o modulo SamplingParameters dentro de Config.jl para criar um arquivo de fluxo por iteração.
--- main.jl
+++ main.jl
@@ -2,40 +2,19 @@ include("src/functions.jl")
 include("config.jl")
 
 using .ConfigParameters
-using .RunningParameters
 const iterations = 100
-for size_of_map_i in list_size_of_map
-    println("----------------------------------")
-    println("---------MAP SIZE ",size_of_map_i,"------------")
-    for tax_rate_i in list_tax_rate
-        println("---------TAX RATE ",tax_rate_i,"------------")
-        
-        for population_rate_i in list_population_ratio
-            global iterations
-            println("------POPULATION RATIO ",population_rate_i,"---------")
 
 
-            println("-------Starting Iterations-------")
+M = define_area(size_of_map)
+M = set_taxes_in_matrix(M, tax_rate)
+M = set_initial_population(M, population_ratio)
+P = size_of_map^2 * population_ratio
 
-            function complete_iteration(iterations)
-                M = define_area(size_of_map_i)
-                M = set_taxes_in_matrix(M,tax_rate_i)
-                M = set_initial_population(M,population_rate_i)
-                for n in 1:iterations
-                    log_iteration(n,iterations)
-                    M = make_iteration(M)
-                    print_matrix(M,n)
-                end
-            end
+println("-------Starting Iterations-------")
 
-            complete_iteration(100)
-
-            println("-----Starting Flux Calculation-----")
-
-            J = (flux_from_file_to_collection(iterations, population_rate_i))
-            print_flux(J,size_of_map_i,tax_rate_i,population_rate_i,iterations)
-
-            println("----------- FINISHED -------------")
-        end
-    end
+for n in 1:iterations
+    global M
+    log_iteration(n, iterations)
+    M = make_iteration(M)
+    print_matrix(M, n)
 end
